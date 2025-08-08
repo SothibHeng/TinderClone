c7aac383099d0fcfb7f1854677af0869d7ee8ef7
Chantrea <sothibheng23@gmail.com>
2025-08-08 08:10:26 +0700

    Create UserSettingController class in controller layer.

---
+++ DatingApp/Controllers/UserSettingController.swift
@@ -0,0 +1,16 @@
+//
+//  UserSettingController.swift
+//  DatingApp
+//
+//  Created by Universe on 8/8/25.
+//
+
+import UIKit
+
+class UserSettingController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+}
