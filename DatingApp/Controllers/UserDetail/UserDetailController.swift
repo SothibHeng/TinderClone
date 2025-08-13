71f0d7f5043ef8904b4582356fa906f7084f050f
Chantrea <sothibheng23@gmail.com>
2025-08-13 09:13:15 +0700

    Create UserDetil folder and create file UserDetailController inside it inside Controller layer .

---
+++ DatingApp/Controllers/UserDetail/UserDetailController.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailController.swift
+//  DatingApp
+//
+//  Created by Universe on 13/8/25.
+//
+
+import UIKit
+
+class UserDetailController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+        
+    }
+}
+
